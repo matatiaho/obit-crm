b128d403b67e34d8831fbbde28d1ecb56ac3d2ea
matatiaho <130637029+matatiaho@users.noreply.github.com>
2025-06-07 12:16:09 -0700

    Improve scrapers

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 from obit_scraper import ObituaryScraper, ObituaryDatabase
 
@@ -6,12 +6,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    search = request.args.get('q', '').strip()
+    sort = request.args.get('sort', 'id')
+
+    sort_map = {
+        'name': 'last_name ASC, first_name ASC',
+        'date': 'date_of_death ASC',
+        'source': 'source ASC',
+        'id': 'id DESC'
+    }
+    order_by = sort_map.get(sort, 'id DESC')
+
     db = ObituaryDatabase()
     cur = db.conn.cursor()
-    cur.execute('SELECT * FROM obituaries ORDER BY id DESC')
+    query = 'SELECT * FROM obituaries'
+    params = []
+    if search:
+        query += ' WHERE first_name LIKE ? OR last_name LIKE ?'
+        like = f'%{search}%'
+        params = [like, like]
+    query += f' ORDER BY {order_by}'
+    cur.execute(query, params)
     rows = cur.fetchall()
     db.close()
-    return render_template('index.html', rows=rows)
+    return render_template('index.html', rows=rows, search=search, sort=sort)
 
 @app.route('/run')
 def run_scraper():
